8704de3f36d0e70d7aa076e55b26e7d08c334a6f
Glen Campbell <glen@pi1.xlerb.com>
2013-10-24 07:49:05 -0500

    iadded RazSnap app

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ razpi
 
 A collection of utilities for the Raspberry Pi
 
+* *razsnap.sh* - simple script that takes a photo with the
+  Pi camera and uploads it to Flickr. Requires the Perl 
+  module Flickr::Upload
---
+++ razsnap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Simple script that takes a snapshot and uploads it to 
+# my Flickr photostream. Requires token, secret on the
+# command line. 
+
+# take the picture
+FILENAME=/tmp/IMG_`date +%Y%m%d%H%M%S`.jpg
+echo file is $FILENAME
+raspistill -q 80 -o $FILENAME
+
+# upload
+# load credentials
+. ~/.flickrrc
+flickr_upload \
+	--auth_token $FLICKR_TOKEN \
+	--key $FLICKR_KEY \
+	--secret $FLICKR_SECRET \
+	--family 1 \
+	--tag='RazSnap' \
+	$FILENAME
+
+# clean up
+rm $FILENAME
